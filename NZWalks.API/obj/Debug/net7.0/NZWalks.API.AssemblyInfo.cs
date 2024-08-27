96fd1be5d589e1827cca4e40ca0916ccd41780ce
Nick <kved0610@mail.ru>
2024-08-27 13:08:49 +0400

    Upload

--- NZWalks.API/obj/Debug/net7.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks.API/obj/Debug/net7.0/NZWalks.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+462893745acc6e8d8c380c07be45b2789d92e12b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f11fa803ca96eb77fd8f8b19b77da7274ed12f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
